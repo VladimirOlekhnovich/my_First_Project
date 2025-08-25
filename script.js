2631947cfee11a46bf866d088770cef8a6d08ec9
VladimirOlekhnovich <volehnovich@bk.ru>
2025-08-25 10:59:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document/addEventListener('DOMContentLoaded', () => {
     }
 
     function validateName(name){
-        const nameRegex = /^[a-zA-Za-яА-яУё\s]{1, 100}$/;
+        const nameRegex = /^[a-zA-Za-яА-яУё\s]{1,100}$/;
         return nameRegex.test(name);
     }
 
@@ -41,4 +41,77 @@ document/addEventListener('DOMContentLoaded', () => {
         const unsafeCodeRegax = /<\/?[a-zA-z][\s\S]*>||<script[\s\S]*?>[\s\S]*?<\/script>||<\?php[\s\S]*?\?>/i;
         return unsafeCodeRegax.test(value);
     }
+
+    function showError(input, errorElement, message){
+        input.classList.add('error');
+            errorElement.textContent = message;
+            errorElement.style.display = 'block';                    
+    }
+
+    function cleanError(input, errorElement){
+        input.classList.remove('error');
+        errorElement.textContent = '';
+        errorElement.style.display = "none";
+    }
+
+    function validateForm(){
+        let isValid = true;
+
+        if(nameInput.value){
+            if(!validateName(nameInput.value)){
+                showError(nameInput, nameError, 'Имя должно содержать только буквы и пробелы и не более 100 символов.')
+                isValid = false;
+            }else{
+                cleanError(nameInput, nameError);
+            }
+        }
+
+        if(emailInput.value){
+            if(!validateEmail(emailInput.value)){
+                showError(emailInput, emailError, 'Пожалуйста, используйте корректный домен для электронной почты')
+                isValid = false;
+            }else{
+                cleanError(emailInput, emailError);
+            }
+        }
+
+        if(messageInput.value){
+            if(!containsUnsafeCode(messageInput.value)){
+                showError(messageInput, messageError, 'Сообщение не должно содержать HTML, JS или PHP код.')
+                isValid = false;
+            }else{
+                cleanError(messageInput, messageError);
+            }
+        }
+
+        return isValid;
+    }
+    
+    loadForbiddenDomains().then(() => {
+        nameInput.addEventListener('input', validateForm);
+        emailInput.addEventListener('input', validateForm);
+        messageInput.addEventListener('input', validateForm);
+
+        form.addEventListener('submit', function(event){
+            event.preventDefault();
+
+            if(validateForm()){
+                alert(`Спасибо, ${nameInput.value}! Ваше сообщение отправлено.`);
+                form.reset();
+            }else{
+                alert('Пожалуйста, исправьте ошибки в форме.');
+            }
+        })
+
+
+        document.querySelectorAll('nav a').forEach(anchor => {
+            anchor.addEventListener('click', function(e){
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                })
+            })
+        })
+    })
+    
 })
